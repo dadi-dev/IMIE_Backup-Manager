23379f266b3412cd21336a8132ac3ccac7eb87e1
crisse14 <51906161+crisse14@users.noreply.github.com>
2019-06-19 15:32:37 +0200

    Add files via upload

--- sav.sh
+++ sav.sh
@@ -1,30 +1,52 @@
 #!/usr/bin/env bash
+# pour le transfert de fichier avec scp nous avons besoin :
+# coté client : de l'emplacement des repertoires, du nom du fichier à envoyer, du nom du propriétaire.
+# coté serveur : du nom du serveur, de son addresse IP, de l'emplacement de destination
+# ici, les les répertoires d'envoie et de reception sont les repertoires des utilisateurs.
 
-	read -p " bonjour, souhaitez vous transférer un fichier? répondez oui ou non : " fct1
+# nous posons un read afin de lancer la fonction de sauvegarde
+				read -p " bonjour, souhaitez vous transférer un fichier? répondez oui ou non : " fct1
 
-		if [ $fct1 != oui ]
-then
-			echo " au revoir "
-exit 1
+							if [ $fct1 != oui ] # si la fonction est non validée par oui
+			then
+										echo " au revoir "
+			exit 1
 fi
-		if [ $fct1 = oui ]
-then
-		read -p " entrez le nom du fichier : " fichier
-		read -p "entrez votre nom d'utilisateur : " nom
-		read -p "entrez le nom du serveur : " serveur
-		read -p "entrez ip du serveur : " ip
+							if [ $fct1 = oui ] # si la fonction est validée par oui
+			then
+# on rempli les conditions d'envoie du fichier à savoir le nom du fichier, le propriétaire, et le destinataire
+#l'ip fixe du destinataire est déterminée suivant son nom de poste.
+				read -p " entrez le nom du fichier : " fichier
+				read -p " entrez votre nom d'utilisateur : " nom
+				read -p " entrez le nom du service julien ou thierry : " serveur
 
-	for variable in $fichier $nom $serveur $ip
+fi
+				for variable in $fichier $nom $serveur $ip
 do
-		echo "$variable"
+										echo "$variable"
 done
-fi
 
-		if [ $ip = 10.5.1.192 ]
+							if [ $serveur = julien ]
+			then
+							ip='10.5.1.169' #ip du poste distant
+							serveur='IMIE-CAEN04' # nom du poste
+
+#transfert du fichier dans le repertoire des utilisateurs avec la commande scp
+#sur un poste windows:
+									scp -r -p /home/$nom/$fichier $serveur@$ip:$fichier
+
+										echo " votre fichier est téléchargé  "
+				exit 1
+fi
+							if [ $serveur = thierry ]
 then
-	scp -r -p /home/$nom/$fichier $serveur@$ip:$fichier
-echo " votre fichier est téléchargé  "
-exit 1
-fi
+							ip='10.5.1.130' #assignation de l'ip du service maintenance
+							serveur='khatz' # nom du poste
 
-	scp -r -p /home/$nom/$fichier $serveur@$ip:/home/$serveur
+#transfert du fichier dans le repertoire des utilisateurs avec la commande scp
+#sur un poste Linux
+									scp -r -p /home/$nom/$fichier $serveur@$ip:/home/$serveur
+
+										echo " votre fichier est téléchargé  "
+				exit 1
+	fi
